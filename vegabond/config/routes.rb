52e6b3f116ee14d0ce96e1ddde1630ebdcbe0c71
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-05 16:08:05 -0700

    git logout added

--- vegabond/config/routes.rb
+++ vegabond/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # sessions routes by phill and adam
   get "/login", to: "sessions#new"
-
+  get "/logout", to: "sessions#destroy"
   post "/sessions", to: "sessions#create"
 end
