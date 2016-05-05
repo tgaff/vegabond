ba9455652df09f11770c2d9d472f2edbc218af80
ilterates <iltates@gmail.com>
2016-05-05 15:34:26 -0700

    user controller functions written

--- vegabond/app/controllers/users_controller.rb
+++ vegabond/app/controllers/users_controller.rb
@@ -6,4 +6,36 @@ class UsersController < ApplicationController
     @users = User.all
 
   end
+
+  def show
+
+    @user = User.find_by_id(params[:id])
+
+  end
+
+  def new
+
+    @user = User.new
+
+  end
+
+  def create
+
+    @user = User.create(user_params)
+  end
+
+  def update
+
+    @user = User.find_by_id(params[:id])
+    @user.update_attributes(user_params)
+  end
+
+  private
+
+  def user_params
+
+    params.require(:user).permit(:first_name,:last_name,:born_city,:current_city,
+    :favorite_destination,:bio,:fav_travel_story,:password,:email)
+
+  end
 end
