36d67ab5b14ffb104f030fb835701c41244da8c2
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-05 15:18:39 -0700

    sessions helper login methods

--- vegabond/config/routes.rb
+++ vegabond/config/routes.rb
@@ -1,56 +1,22 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # page handled by phill and adam
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  def logged_in?
+    if current_user == nil
+      redirect_to new_session_path
+    end
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  def logout
+    @current_user = session[:user_id] = nil
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
