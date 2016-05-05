42e7e10337bd860c53097847cfaf4235931c5826
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-05 15:58:25 -0700

    added create method sessions ctrl

--- vegabond/app/controllers/sessions_controller.rb
+++ vegabond/app/controllers/sessions_controller.rb
@@ -5,4 +5,21 @@ class SessionsController < ApplicationController
     @user = User.new
     render :new
   end
+
+  def create
+    @user = User.confirm(user_params)
+    if @user
+      login(@user)
+      redirect_to @user
+    else
+      redirect_to login_path
+    end
+  end
+
+
+  # private user_params method by phill and adam
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
