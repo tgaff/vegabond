14374173a3e8ca61bb3f34e1d689641ce5c24c9e
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-06 12:22:34 -0700

    schema and user ref to posts added

--- vegabond/db/schema.rb
+++ vegabond/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506191236) do
+ActiveRecord::Schema.define(version: 20160506192053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160506191236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "author"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -40,4 +43,5 @@ ActiveRecord::Schema.define(version: 20160506191236) do
     t.datetime "updated_at",           null: false
   end
 
+  add_foreign_key "posts", "users"
 end
