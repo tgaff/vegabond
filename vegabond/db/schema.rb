27fc84dca69746fda35518ec8a3bcf485314274f
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-06 12:14:15 -0700

    schema updated to included author

--- vegabond/db/schema.rb
+++ vegabond/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506175111) do
+ActiveRecord::Schema.define(version: 20160506191236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160506175111) do
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
   end
 
   create_table "users", force: :cascade do |t|
