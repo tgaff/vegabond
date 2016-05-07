5d782f0be23a9c8e5379297ff9106651fe9287d9
ilterates <iltates@gmail.com>
2016-05-07 13:47:29 -0700

    Added a feature where if user goes to wrong direction, they get redirected to root. it's currently commented out for production

--- vegabond/config/routes.rb
+++ vegabond/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
  root to: "users#index"
+ # get '*path' => redirect('/') # to be enabled after developement
  # created by ilter and marcelo
  get "/users", to: "users#index", as: "users"
  get "/users/new", to: "users#new", as: "new_user"
--- vegabond/db/schema.rb
+++ vegabond/db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160507184251) do
 
-
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
