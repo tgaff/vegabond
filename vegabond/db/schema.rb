62a8fde9deb03f0c65c0262dcff4d364178ec033
PJC-1 <jumpzine@gmail.com>
2016-05-07 11:28:13 -0700

    created cities model and updated the schema

--- vegabond/db/schema.rb
+++ vegabond/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506192053) do
+ActiveRecord::Schema.define(version: 20160507182203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +47,10 @@ ActiveRecord::Schema.define(version: 20160506192053) do
     t.string   "email"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "posts", "users"
