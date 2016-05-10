b05b9bef92cb380c22681b883557f6deda242171
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-09 19:29:40 -0700

    images linked in from imgur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
     @posts = @user.posts
-    post = Post.find_by_id(params[:id])
+    # post = Post.find_by_id(params[:id])
     @full_background = false
   end
 
