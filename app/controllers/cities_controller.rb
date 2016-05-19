a84ef2da4e022207675010a498506f32d35c8c41
tgaff <tgaff@alumni.nd.edu>
2016-05-19 10:13:05 -0700

    refactor city controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,20 +2,7 @@ class CitiesController < ApplicationController
 
 
   def show
-    @user = User.find_by_id(params[:id])
     @city = City.find_by_id(params[:id])
-    @post = Post.find_by_id(params[:id])
-    if @city[:id] == 1
-      @cityclass = "sanfrancisco"
-    elsif @city[:id] == 2
-      @cityclass = "valdivia"
-    elsif @city[:id] == 3
-      @cityclass = "melbourne"
-    else
-      @cityclass = "istanbul"
-    end
-    @cityname = "SF"
-    gon.cityname = @cityname
+    gon.cityname = @city.name
   end
-
 end
